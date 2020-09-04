78c48e509493032a51db80ac7bd53c4fdae30828
njbsb <49741741+njbsb@users.noreply.github.com>
2020-09-05 05:06:51 +0800

    changed text and added reminder

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,7 +5,7 @@
     <h1 class="display-3">Welcome to SIG Integrated System!</h1>
     <p class="lead">This is a website to manage VIC System.</p>
     <hr class="my-4">
-    <p>Extra text here.</p>
+    <p>Insert Leaderboard/Dashboard here.</p>
     <p class="lead">
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </p>
