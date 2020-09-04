97611fd217dacdd255b4a55915fded0150ae875f
njbsb <49741741+njbsb@users.noreply.github.com>
2020-09-05 05:06:37 +0800

    added edit profile for both user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,11 +5,26 @@
             $data['title'] = 'User profile';
             
             $this->load->view('templates/header');
-            $this->load->view('user/index', $data);
+            $this->load->view('user/student/profile', $data);
+            // put if statement to check user type and load the right view
             $this->load->view('templates/footer');
         }
 
-        public function update_profile() {
+        public function edit() {
+            $data['title'] = 'Update Student user';
+            $this->load->view('templates/header');
+            if(1) {
+                // load mentor/user model's data
+                // load the right view based on user type
+            }
+            $this->load->view('user/student/update', $data);
+            $this->load->view('templates/footer');
+            
+            // NOT DONE
+            // put if condition to check the user type of the user
+            // so that we can load the right view
+            
+            // DONE
             // replace profile controller with user controller
             // later re route using config route
         }
