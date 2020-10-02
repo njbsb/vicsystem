c7d830c7a75399c50b031bea1a993e9155f40dba
njbsb <49741741+njbsb@users.noreply.github.com>
2020-10-02 16:10:33 +0800

    updated database design

--- assets/database/sigsystem_db.sql
+++ assets/database/sigsystem_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2020 at 03:09 PM
+-- Generation Time: Oct 02, 2020 at 10:09 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -62,8 +62,9 @@ CREATE TABLE `tbl_academicsession` (
 
 INSERT INTO `tbl_academicsession` (`id`, `acadyear_id`, `semester_id`, `status`) VALUES
 (1, 1, 1, 'active'),
-(2, 1, 2, 'active'),
-(3, 1, 3, 'inactive');
+(2, 1, 2, 'inactive'),
+(3, 1, 3, 'inactive'),
+(4, 2, 1, 'inactive');
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,8 @@ CREATE TABLE `tbl_academicyear` (
 INSERT INTO `tbl_academicyear` (`id`, `acadyear`, `status`) VALUES
 (1, '2016/2017', 'active'),
 (2, '2017/2018', 'inactive'),
-(3, '2018/2019', 'inactive');
+(3, '2018/2019', 'inactive'),
+(4, '2019/2020', 'inactive');
 
 -- --------------------------------------------------------
 
@@ -118,11 +120,13 @@ CREATE TABLE `tbl_activity` (
 
 INSERT INTO `tbl_activity` (`id`, `activity_name`, `activity_desc`, `acadsession_id`, `sig_id`, `advisor_matric`, `datetime_start`, `datetime_end`, `venue`, `theme`, `actbudget_id`, `author_matric`, `slug`, `photo_path`, `paperwork_file`, `created_at`, `updated_at`) VALUES
 (1, 'VIC Bonding 2016', '<p>Bonding session with the member of the club at Port Dickson, Negeri Sembilan</p>\r\n', 1, 1, 'K003', '2020-08-01 08:26:28', '2020-08-01 16:26:28', NULL, '', NULL, NULL, 'VIC-Bonding-2016', '', '', '2020-09-02 07:06:21', '2020-09-04 08:47:39'),
-(3, 'Meeting 1234', '<p>hehe</p>\r\n', 1, 4, 'K003', '2020-09-02 22:45:00', '2020-09-10 22:45:00', '', '', NULL, NULL, 'Meeting-1234', '', '', '2020-09-06 07:05:46', '2020-09-04 08:47:39'),
+(3, 'Meeting 1234', '<p>hehe</p>\r\n', 1, 4, 'K003', '2020-09-02 22:45:00', '2020-09-10 22:45:00', 'MR 1', 'Meeting sampai esok', NULL, NULL, 'Meeting-1234', '', '', '2020-09-06 07:05:46', '2020-09-04 08:47:39'),
 (4, 'Onboarding with HRM', '<p>Invite new intern</p>\r\n', 2, 6, 'K001', '2020-09-10 00:01:00', '2020-09-19 00:01:00', NULL, '', NULL, NULL, 'Onboarding-with-HRM', '', '', '2020-09-06 07:05:46', '2020-09-04 08:47:39'),
 (5, 'Meeting 4', '<p>meettttt bersama tunku taufiq</p>\r\n', 1, 10, 'K003', '2020-09-04 00:17:00', '2020-09-05 00:17:00', NULL, '', NULL, NULL, 'Meeting-4', '', '', '2020-09-06 07:05:46', '2020-09-04 08:47:39'),
 (6, 'Lawatan to Media Prima', '<p>This visit to media prima is to enhance our club member on how media production is conducted within the real world</p>\r\n', 1, 1, 'K001', '2020-09-03 11:37:00', '2020-09-04 11:37:00', 'Media Prima', 'Biar orang buat kita, kita buat website', NULL, NULL, 'Lawatan-to-Media-Prima', '', '', '2020-09-06 07:05:46', '2020-09-04 08:47:39'),
-(8, 'Sample Activity', '<p>In a small group, you will create a raft from an 8-inch-by-8-inch piece of aluminum foil and four plastic straws; the goal for the raft is to hold as many pennies as possible. All materials will be provided for you, and you cannot use any outside materials.</p>\r\n', 1, 1, 'K004', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, '', NULL, NULL, 'Sample-Activity', '', '', '2020-09-06 08:01:39', '2020-09-06 08:01:39');
+(8, 'Sample Activity', '<p>In a small group, you will create a raft from an 8-inch-by-8-inch piece of aluminum foil and four plastic straws; the goal for the raft is to hold as many pennies as possible. All materials will be provided for you, and you cannot use any outside materials.</p>\r\n', 1, 1, 'K004', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, '', NULL, NULL, 'Sample-Activity', '', '', '2020-09-06 08:01:39', '2020-09-06 08:01:39'),
+(9, 'Dinner', '<p>Dinner with the whole batch of the club</p>\r\n', 1, 1, 'K003', '2020-10-02 20:00:00', '2020-10-02 20:30:00', 'Tenera Tenera 2', 'Maqon', NULL, NULL, 'Dinner', '', '', '2020-10-02 07:19:49', '2020-10-02 07:19:49'),
+(10, 'Dinner 2.0', '<p>Dinner 2.0</p>\r\n', 1, 1, 'K001', '2020-10-02 16:05:00', '2020-10-03 16:05:00', 'BGR', 'takde', NULL, NULL, 'Dinner-20', '', '', '2020-10-02 08:03:25', '2020-10-02 08:03:25');
 
 -- --------------------------------------------------------
 
@@ -145,8 +149,8 @@ CREATE TABLE `tbl_activity_budget` (
 
 CREATE TABLE `tbl_activity_committee` (
   `activity_id` int(11) DEFAULT NULL,
-  `student_matric` varchar(8) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
+  `student_matric` varchar(8) DEFAULT NULL,
   `role_desc` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -154,10 +158,16 @@ CREATE TABLE `tbl_activity_committee` (
 -- Dumping data for table `tbl_activity_committee`
 --
 
-INSERT INTO `tbl_activity_committee` (`activity_id`, `student_matric`, `role_id`, `role_desc`) VALUES
-(1, 'A160000', 11, 'Makanan'),
-(6, NULL, 9, ''),
-(1, 'A160001', 5, '');
+INSERT INTO `tbl_activity_committee` (`activity_id`, `role_id`, `student_matric`, `role_desc`) VALUES
+(1, 11, 'A160000', 'Makanan'),
+(6, 9, NULL, ''),
+(1, 5, 'A160001', ''),
+(9, 5, NULL, ''),
+(9, 6, NULL, ''),
+(9, 9, NULL, ''),
+(10, 5, 'A160003', ''),
+(10, 6, 'A160002', ''),
+(10, 9, 'A160001', '');
 
 -- --------------------------------------------------------
 
@@ -221,7 +231,8 @@ CREATE TABLE `tbl_citra_registration` (
 
 INSERT INTO `tbl_citra_registration` (`student_matric`, `acadsession_id`, `citra_code`, `grade_id`) VALUES
 ('A160000', 1, 'LMCK1331', NULL),
-('A160000', 1, 'LMCK1421', NULL);
+('A160000', 1, 'LMCK1421', NULL),
+('A160001', 1, 'LMCK1531', NULL);
 
 -- --------------------------------------------------------
 
@@ -245,10 +256,10 @@ CREATE TABLE `tbl_collaborator` (
 CREATE TABLE `tbl_comment` (
   `activity_id` int(11) NOT NULL,
   `student_matric` varchar(8) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
   `comment` varchar(255) NOT NULL,
   `commented_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `category_id` int(11) DEFAULT 1
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -257,15 +268,16 @@ CREATE TABLE `tbl_comment` (
 
 INSERT INTO `tbl_comment` (`activity_id`, `student_matric`, `role_id`, `comment`, `commented_at`, `category_id`) VALUES
 (1, 'A160000', 5, 'Best', '2020-09-03 05:39:46', 1),
-(1, '', 9, 'HUHUHU', '2020-09-03 05:39:46', 1),
-(6, '', 12, '<p>komenkomen</p>\r\n', '2020-09-03 06:37:03', 1),
-(6, '', 12, 'try', '2020-09-03 09:24:29', 1),
-(6, '', 12, 'try', '2020-09-03 09:24:32', 1),
-(4, '', 12, 'test onboarding', '2020-09-03 09:31:45', 1),
-(8, '', 12, 'Sample comment', '2020-09-06 08:02:18', 1),
-(6, '', 12, 'pass', '2020-09-23 08:16:13', 1),
-(6, '', 12, 'helo', '2020-09-23 08:21:15', 1),
-(3, 'A160000', 12, 'No food dowan come', '2020-09-28 16:31:47', 1);
+(1, 'A160002', 9, 'HUHUHU', '2020-09-03 05:39:46', 1),
+(6, 'A160002', 12, '<p>komenkomen</p>\r\n', '2020-09-03 06:37:03', 1),
+(6, 'A160002', 12, 'try', '2020-09-03 09:24:29', 1),
+(6, 'A160002', 12, 'try', '2020-09-03 09:24:32', 1),
+(4, 'A160002', 12, 'test onboarding', '2020-09-03 09:31:45', 1),
+(8, 'A160002', 12, 'Sample comment', '2020-09-06 08:02:18', 1),
+(6, 'A160002', 12, 'pass', '2020-09-23 08:16:13', 1),
+(6, 'A160002', 12, 'helo', '2020-09-23 08:21:15', 1),
+(3, 'A160000', 12, 'No food dowan come', '2020-09-28 16:31:47', 1),
+(6, 'A160000', NULL, 'Venue isnt good', '2020-10-01 22:31:23', 2);
 
 -- --------------------------------------------------------
 
@@ -347,7 +359,7 @@ CREATE TABLE `tbl_levelscore` (
 INSERT INTO `tbl_levelscore` (`id`, `level`, `percentage`) VALUES
 (1, 'A1', 15),
 (2, 'A2', 15),
-(3, 'B', 10),
+(3, 'B1', 10),
 (4, 'COMP', 15);
 
 -- --------------------------------------------------------
@@ -368,7 +380,7 @@ CREATE TABLE `tbl_mentor` (
 --
 
 INSERT INTO `tbl_mentor` (`matric`, `position`, `roomnum`, `orgrole_id`) VALUES
-('K001', 'Head of CAIT', '', 1),
+('K001', 'Head of CAIT', 'E-12-12', 1),
 ('K002', 'Head of Masters Programme', 'E-1-2', 2),
 ('K003', 'Teaching Instructor', '', 1),
 ('K004', 'Dr Astronomy', '', 1),
@@ -470,6 +482,30 @@ INSERT INTO `tbl_scattendance` (`id`, `name`, `score`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_scdigitalcv`
+--
+
+CREATE TABLE `tbl_scdigitalcv` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_scdigitalcv`
+--
+
+INSERT INTO `tbl_scdigitalcv` (`id`, `name`, `score`) VALUES
+(1, 'Very attractive design and very informative', 5),
+(2, 'Attractive design and informative', 4),
+(3, 'Moderate design and information', 3),
+(4, 'No activity of current year', 2),
+(5, 'No update since previous year', 1),
+(6, 'No digital CV', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_scinvolvement`
 --
 
@@ -488,6 +524,29 @@ INSERT INTO `tbl_scinvolvement` (`id`, `name`, `score`) VALUES
 (2, 'Moderately Active', 5),
 (3, 'Less Active', 3),
 (4, 'Not Active', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_scleadership`
+--
+
+CREATE TABLE `tbl_scleadership` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_scleadership`
+--
+
+INSERT INTO `tbl_scleadership` (`id`, `name`, `score`) VALUES
+(1, 'Very good', 5),
+(2, 'Good', 4),
+(3, 'Moderate', 3),
+(4, 'Very little', 1),
+(5, 'No leadership', 0);
 
 -- --------------------------------------------------------
 
@@ -517,7 +576,6 @@ INSERT INTO `tbl_scmeeting` (`id`, `name`, `score`) VALUES
 --
 
 CREATE TABLE `tbl_scorecomp` (
-  `id` int(11) NOT NULL,
   `acadsession_id` int(11) NOT NULL,
   `student_matric` varchar(8) NOT NULL,
   `marker_matric` varchar(8) NOT NULL,
@@ -527,6 +585,13 @@ CREATE TABLE `tbl_scorecomp` (
   `sc_volunteer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_scorecomp`
+--
+
+INSERT INTO `tbl_scorecomp` (`acadsession_id`, `student_matric`, `marker_matric`, `levelscore_id`, `sc_digitalcv`, `sc_leadership`, `sc_volunteer`) VALUES
+(1, 'A160000', 'K001', 4, 2, 2, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -534,17 +599,25 @@ CREATE TABLE `tbl_scorecomp` (
 --
 
 CREATE TABLE `tbl_scorelevel` (
-  `id` int(11) NOT NULL,
   `acadsession_id` int(11) NOT NULL,
   `student_matric` varchar(8) NOT NULL,
   `marker_matric` varchar(8) NOT NULL,
   `activity_id` int(11) NOT NULL,
   `levelscore_id` int(11) NOT NULL,
-  `sc_position` int(11) NOT NULL,
-  `sc_meeting` int(11) NOT NULL,
-  `sc_attendance` int(11) NOT NULL,
-  `sc_involvement` int(11) NOT NULL
+  `sc_position` int(11) DEFAULT NULL,
+  `sc_meeting` int(11) DEFAULT NULL,
+  `sc_attendance` int(11) DEFAULT NULL,
+  `sc_involvement` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_scorelevel`
+--
+
+INSERT INTO `tbl_scorelevel` (`acadsession_id`, `student_matric`, `marker_matric`, `activity_id`, `levelscore_id`, `sc_position`, `sc_meeting`, `sc_attendance`, `sc_involvement`) VALUES
+(1, 'A160000', 'K001', 6, 1, 2, 5, NULL, NULL),
+(1, 'A160000', 'K001', 5, 2, 4, NULL, NULL, NULL),
+(1, 'A160000', 'K001', 3, 3, 3, NULL, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -642,7 +715,9 @@ CREATE TABLE `tbl_student` (
 INSERT INTO `tbl_student` (`matric`, `phonenum`, `program_code`, `joined_sig`, `mentor_matric`) VALUES
 ('A123', '0011', 'SEIS', NULL, NULL),
 ('A160000', '0123456789', 'SEMM', NULL, 'K004'),
-('A160001', '0123334444', 'IT', NULL, 'K003');
+('A160001', '0123334444', 'SEIS', NULL, 'K003'),
+('A160002', '011-2228888', 'SEMM', NULL, 'K003'),
+('A160003', '013-1212333', 'IT', NULL, 'K001');
 
 -- --------------------------------------------------------
 
@@ -670,10 +745,10 @@ CREATE TABLE `tbl_user` (
 INSERT INTO `tbl_user` (`id`, `name`, `email`, `password`, `usertype_id`, `sig_id`, `profile_image`, `created_at`, `userstatus_id`, `dob`) VALUES
 ('A160000', 'Khairul Anu AR', 'a160000@siswa.my', 'password', 3, 1, '', '2020-09-04 08:14:26', 2, NULL),
 ('A160001', 'Farhan Kamal', 'A160001@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 3, 1, '', '2020-09-26 10:42:51', 2, '1992-06-06'),
-('A160002', 'A160002', 'A160002@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 3, 1, '', '2020-09-26 11:03:24', 1, NULL),
-('A160003', 'A160003', 'A160003@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 3, 1, '', '2020-09-26 11:05:18', 1, NULL),
+('A160002', 'Minmin', 'A160002@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 3, 1, '', '2020-09-26 11:03:24', 2, '1997-05-05'),
+('A160003', 'YinYin', 'A160003@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 3, 1, '', '2020-09-26 11:05:18', 2, '1999-12-02'),
 ('admin', 'Admin1', 'admin@gmail.com', 'admin123', 1, NULL, '', '2020-09-07 07:39:53', 2, NULL),
-('K001', 'Dr Syahanim bt', 'K001@ukm.my', '5f4dcc3b5aa765d61d8327deb882cf99', 2, 1, 'anim.png', '2020-09-26 11:06:40', 2, NULL),
+('K001', 'Dr Syahanim bte', 'syahanim@ukm.my', '5f4dcc3b5aa765d61d8327deb882cf99', 2, 1, 'anim.png', '2020-09-26 11:06:40', 2, NULL),
 ('K002', 'Assoc Prof. Dr. Tengku Meriam', 'tsmeriam@ukm.edu.my', 'password', 2, 1, 'tengku.png', '2020-09-26 19:15:31', 2, NULL),
 ('K003', 'Masura bt Rahmat', 'masura@ukm.edu.my', 'password', 2, 1, 'masura.png', '2020-09-26 19:14:23', 2, NULL),
 ('K004', 'Dr AffenD', 'k004@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 2, 1, '', '2020-09-27 05:17:13', 2, '1996-05-05'),
@@ -764,9 +839,9 @@ ALTER TABLE `tbl_activity_budget`
 -- Indexes for table `tbl_activity_committee`
 --
 ALTER TABLE `tbl_activity_committee`
-  ADD KEY `tbl_activity_committee_ibfk_1` (`activity_id`),
   ADD KEY `student_matric` (`student_matric`),
-  ADD KEY `role_id` (`role_id`);
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `tbl_activity_committee_ibfk_1` (`activity_id`);
 
 --
 -- Indexes for table `tbl_admin`
@@ -800,9 +875,9 @@ ALTER TABLE `tbl_collaborator`
 --
 ALTER TABLE `tbl_comment`
   ADD KEY `activity_id` (`activity_id`),
-  ADD KEY `role_id` (`role_id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `tbl_comment_ibfk_2` (`student_matric`);
+  ADD KEY `tbl_comment_ibfk_2` (`student_matric`),
+  ADD KEY `tbl_comment_ibfk_4` (`category_id`),
+  ADD KEY `tbl_comment_ibfk_3` (`role_id`);
 
 --
 -- Indexes for table `tbl_comment_category`
@@ -863,9 +938,21 @@ ALTER TABLE `tbl_scattendance`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_scdigitalcv`
+--
+ALTER TABLE `tbl_scdigitalcv`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_scinvolvement`
 --
 ALTER TABLE `tbl_scinvolvement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_scleadership`
+--
+ALTER TABLE `tbl_scleadership`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -878,7 +965,6 @@ ALTER TABLE `tbl_scmeeting`
 -- Indexes for table `tbl_scorecomp`
 --
 ALTER TABLE `tbl_scorecomp`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `levelscore_id` (`levelscore_id`),
   ADD KEY `student_matric` (`student_matric`),
   ADD KEY `marker_matric` (`marker_matric`),
@@ -888,7 +974,6 @@ ALTER TABLE `tbl_scorecomp`
 -- Indexes for table `tbl_scorelevel`
 --
 ALTER TABLE `tbl_scorelevel`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `activity_id` (`activity_id`),
   ADD KEY `student_matric` (`student_matric`),
   ADD KEY `marker_matric` (`marker_matric`),
@@ -950,19 +1035,19 @@ ALTER TABLE `tbl_usertype`
 -- AUTO_INCREMENT for table `tbl_academicsession`
 --
 ALTER TABLE `tbl_academicsession`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_academicyear`
 --
 ALTER TABLE `tbl_academicyear`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_activity`
 --
 ALTER TABLE `tbl_activity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_activity_budget`
@@ -1007,28 +1092,28 @@ ALTER TABLE `tbl_scattendance`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `tbl_scdigitalcv`
+--
+ALTER TABLE `tbl_scdigitalcv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tbl_scinvolvement`
 --
 ALTER TABLE `tbl_scinvolvement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `tbl_scleadership`
+--
+ALTER TABLE `tbl_scleadership`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `tbl_scmeeting`
 --
 ALTER TABLE `tbl_scmeeting`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tbl_scorecomp`
---
-ALTER TABLE `tbl_scorecomp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_scorelevel`
---
-ALTER TABLE `tbl_scorelevel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_scposition`
@@ -1091,7 +1176,7 @@ ALTER TABLE `tbl_activity`
 -- Constraints for table `tbl_activity_committee`
 --
 ALTER TABLE `tbl_activity_committee`
-  ADD CONSTRAINT `tbl_activity_committee_ibfk_1` FOREIGN KEY (`activity_id`) REFERENCES `tbl_activity` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_activity_committee_ibfk_1` FOREIGN KEY (`activity_id`) REFERENCES `tbl_activity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `tbl_activity_committee_ibfk_2` FOREIGN KEY (`student_matric`) REFERENCES `tbl_student` (`matric`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `tbl_activity_committee_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `tbl_role` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -1110,8 +1195,8 @@ ALTER TABLE `tbl_citra_registration`
 ALTER TABLE `tbl_comment`
   ADD CONSTRAINT `tbl_comment_ibfk_1` FOREIGN KEY (`activity_id`) REFERENCES `tbl_activity` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `tbl_comment_ibfk_2` FOREIGN KEY (`student_matric`) REFERENCES `tbl_student` (`matric`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbl_comment_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `tbl_role` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbl_comment_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `tbl_comment_category` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `tbl_comment_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `tbl_role` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_comment_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `tbl_comment_category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_mentor`
