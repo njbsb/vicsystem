e5a5736826c9138a082e6b875314340b1e078cc4
najib s <49741741+njbsb@users.noreply.github.com>
2021-03-07 17:46:50 +0800

    code update for organization view

--- application/controllers/Organization.php
+++ application/controllers/Organization.php
@@ -10,15 +10,46 @@ class Organization extends CI_Controller
         $sig = $this->sig_model->get_sig($sig_id);
         $activeacadyear = $this->committee_model->get_activeacadyear();
         $acadyear_id = $activeacadyear['id'];
+
+        $president = $this->committee_model->get_president($sig_id, $acadyear_id);
+        $deputypresident = $this->committee_model->get_deputypresident($sig_id, $acadyear_id);
+        $treasurer = $this->committee_model->get_orgtreasurer($sig_id, $acadyear_id);
+        $secretary = $this->committee_model->get_orgsecretary($sig_id, $acadyear_id);
+
+        $secondrows = array(
+            'secretary' => $secretary, 
+            'deputypresident' => $deputypresident, 
+            'treasurer' => $treasurer);
+        $secondrowid = array('secretary', 'deputypresident', 'treasurer');
+        $secondrownames = array('Secretary', 'Deputy President', 'Treasurer');
+        foreach($secondrowid as $key=>$name) {
+            if(!$secondrows[$name]) {
+                $secondrows[$name] = array(
+                    'rolename' => $secondrownames[$key],
+                    'profile_image' => '',
+                    'student_matric' => 'no data',
+                    'name' => '-',
+                    'email' => '-'
+                );
+            }
+        }
+        // print_r($secondrows);
+        if(!$president) {
+            $president = array(
+                'rolename' => 'President',
+                'profile_image' => '',
+                'student_matric' => 'no data',
+                'name' => '-',
+                'email' => '-'
+            );
+        }
         $data = array(
             'title' => 'Organization Hierarchy',
             'sig' => $sig,
             'isMentor' => $this->session->userdata('isMentor'),
             'activeacadyear' => $activeacadyear,
-            'president' => $this->committee_model->get_president($sig_id, $acadyear_id),
-            'deputypresident' => $this->committee_model->get_deputypresident($sig_id, $acadyear_id),
-            'treasurer' => $this->committee_model->get_orgtreasurer($sig_id, $acadyear_id),
-            'secretary' => $this->committee_model->get_orgsecretary($sig_id, $acadyear_id),
+            'president' => $president,
+            'secondrows' => $secondrows,
             'orgajks' => $this->committee_model->get_ajks($sig_id, $acadyear_id),
             'sigcommittees' => $this->committee_model->get_orgcommittee($sig_id),
             'roles_org' => $this->committee_model->get_roles_org(), # to register new com
--- application/views/organization/index.php
+++ application/views/organization/index.php
@@ -3,124 +3,92 @@
 <h4 class="text-center"><?= $activeacadyear['acadyear'] ?></h4>
 
 <?php if ($president) : ?>
-    <div class="row text-center">
-        <div class="col-md-4 offset-md-4">
-            <div class="card mb-4">
-                <h4 class="card-header text-white bg-dark">
-                    <?= $president['rolename'] ?>
-                </h4>
-                <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $president['profile_image']); ?>" alt="<?= $president['profile_image']; ?>">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="<?= site_url('/student/' . $president['student_matric']) ?>"><?= $president['name'] ?></a>
-                    </h5>
-                    <h6 class="card-subtitle text-muted"><?= $president['student_matric']; ?></h6>
-                </div>
-                <div class="card-footer text-muted">
-                    <?= $president['email'] ?>
-                </div>
+<div class="row text-center">
+    <div class="col-md-4 offset-md-4">
+        <div class="card mb-4">
+            <h4 class="card-header text-white bg-dark">
+                <?= $president['rolename'] ?>
+            </h4>
+            <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $president['profile_image']); ?>"
+                alt="<?= $president['profile_image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <?php if($president['name'] == '-'): ?>
+                    <a href="#"><?= $president['name'] ?></a>
+                    <?php else: ?>
+                    <a href="<?= site_url('/student/' . $president['student_matric']) ?>"><?= $president['name'] ?></a>
+                    <?php endif ?>
+                </h5>
+                <h6 class="card-subtitle text-muted"><?= $president['student_matric']; ?></h6>
+            </div>
+            <div class="card-footer text-muted">
+                <?= $president['email'] ?>
             </div>
         </div>
     </div>
-<?php else : ?>
-    <p class="text-center">No President for the current year</p>
+</div>
 <?php endif ?>
 
 <div class="row text-center">
+    <?php foreach ($secondrows as $rows): ?>
     <div class="col-md-4">
-        <?php if ($secretary) : ?>
-            <div class="card mb-4">
-                <h4 class="card-header text-white bg-dark">
-                    <?= $secretary['rolename'] ?>
-                </h4>
-                <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $secretary['profile_image']); ?>" alt="<?= $secretary['profile_image']; ?>">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="<?= site_url('/student/' . $secretary['student_matric']) ?>"><?= $secretary['name'] ?></a>
-                    </h5>
-                    <h6 class="card-subtitle text-muted"><?= $secretary['student_matric']; ?></h6>
-                </div>
-                <div class="card-footer text-muted">
-                    <?= $secretary['email'] ?>
-                </div>
+        <div class="card mb-4">
+            <h4 class="card-header text-white bg-dark">
+                <?= $rows['rolename'] ?>
+            </h4>
+            <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $rows['profile_image']); ?>"
+                alt="<?= $rows['profile_image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <?php if($rows['name'] != '-'): ?>
+                    <a href="<?= site_url('/student/' . $rows['student_matric']) ?>"><?= $rows['name'] ?></a>
+                    <?php else: ?>
+                    <a href="#"><?= $rows['name'] ?></a>
+                    <?php endif ?>
+
+                </h5>
+                <h6 class="card-subtitle text-muted"><?= $rows['student_matric']; ?></h6>
             </div>
-        <?php else : ?>
-            <p class="text-center">No secretary for the current year</p>
-        <?php endif ?>
-    </div>
-    <div class="col-md-4">
-        <?php if ($deputypresident) : ?>
-            <div class="card mb-4">
-                <h4 class="card-header text-white bg-dark">
-                    <?= $deputypresident['rolename']; ?>
-                </h4>
-                <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $deputypresident['profile_image']); ?>" alt="<?= $deputypresident['profile_image']; ?>">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="<?= site_url('/student/' . $deputypresident['student_matric']) ?>"><?= $deputypresident['name'] ?></a>
-                    </h5>
-                    <h6 class="card-subtitle text-muted"><?= $deputypresident['student_matric']; ?></h6>
-                </div>
-                <div class="card-footer text-muted">
-                    <?= $deputypresident['email'] ?>
-                </div>
+            <div class="card-footer text-muted">
+                <?= $rows['email'] ?>
             </div>
-        <?php else : ?>
-            <p class="text-center">No deputy president for the current year</p>
-        <?php endif ?>
+        </div>
     </div>
-    <div class="col-md-4">
-        <?php if ($treasurer) : ?>
-            <div class="card mb-4">
-                <h4 class="card-header text-white bg-dark">
-                    <?= $treasurer['rolename']; ?>
-                </h4>
-                <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $treasurer['profile_image']); ?>" alt="<?= $treasurer['profile_image']; ?>">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="<?= site_url('/student/' . $treasurer['student_matric']) ?>">
-                            <?= $treasurer['name'] ?>
-                        </a>
-                    </h5>
-                    <h6 class="card-subtitle text-muted"><?= $treasurer['student_matric']; ?></h6>
-                </div>
-                <div class="card-footer text-muted">
-                    <?= $treasurer['email'] ?>
-                </div>
-            </div>
-        <?php else : ?>
-            <p class="text-center">No treasurer for the current year</p>
-        <?php endif ?>
-    </div>
+    <?php endforeach ?>
+
 </div>
 <hr>
 <div class="row text-center justify-content-center">
     <?php if ($orgajks) : ?>
-        <?php foreach ($orgajks as $ajk) : ?>
-            <div class="col-md-3">
-                <div class="card mb-3">
-                    <h4 class="card-header text-white bg-dark">
-                        <?= $ajk['rolename']; ?>
-                    </h4>
-                    <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $ajk['profile_image']); ?>" alt="<?= $ajk['profile_image']; ?>">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            <a href="<?= site_url('/student/' . $ajk['student_matric']) ?>">
-                                <?= $ajk['name'] ?>
-                            </a>
-                        </h5>
-                        <h6 class="card-subtitle text-muted"><?= $ajk['student_matric']; ?></h6>
-                    </div>
-                    <div class="card-footer text-muted">
-                        <?= $ajk['email'] ?>
-                    </div>
-                </div>
+    <?php foreach ($orgajks as $ajk) : ?>
+    <div class="col-md-3">
+        <div class="card mb-3">
+            <div class="card-header bg-dark">
+                <h4 class="text-white ">
+                    <?= $ajk['rolename']; ?>
+                </h4>
+                <span class="badge badge-pill badge-light"><?= $ajk['role_desc'] ?></span>
             </div>
-        <?php endforeach ?>
-    <?php else : ?>
-        <div class="col-md-4 offset-md-4">
-            <p class="text-center">No AJK for the current year</p>
+            <img style="max-height:240px; max-width:auto; display: block; object-fit:cover;  padding:10px;" src="<?= base_url('assets/images/profile/' . $ajk['profile_image']); ?>"
+                alt="<?= $ajk['profile_image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <a href="<?= site_url('/student/' . $ajk['student_matric']) ?>">
+                        <?= $ajk['name'] ?>
+                    </a>
+                </h5>
+                <h6 class="card-subtitle text-muted"><?= $ajk['student_matric']; ?></h6>
+            </div>
+            <div class="card-footer text-muted">
+                <?= $ajk['email'] ?>
+            </div>
         </div>
+    </div>
+    <?php endforeach ?>
+    <?php else : ?>
+    <div class="col-md-4 offset-md-4">
+        <p class="text-center">No AJK for the current year</p>
+    </div>
     <?php endif ?>
 </div>
 <hr>
@@ -128,7 +96,7 @@
     <!-- insert members here -->
 </div>
 <?php if ($isMentor) : ?>
-    <button data-toggle="modal" data-target="#registercommittee" class="btn btn-outline-primary margin">Add new committee</button>
+<button data-toggle="modal" data-target="#registercommittee" class="btn btn-outline-primary margin">Add new committee</button>
 <?php endif ?>
 <table id="orgcom_table" class="table table-hover">
     <thead class="table-dark">
@@ -137,20 +105,21 @@
             <th>Name</th>
             <th>Role</th>
             <?php if ($this->session->userdata('isMentor')) : ?>
-                <th>Action</th>
+            <th>Action</th>
             <?php endif ?>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($sigcommittees as $sigcom) : ?>
-            <tr>
-                <td><?= $sigcom['student_matric'] ?></td>
-                <td><?= $sigcom['name'] ?></td>
-                <td><?= $sigcom['rolename'] ?></td>
-                <?php if ($this->session->userdata('isMentor')) : ?>
-                    <td><a class="btn btn-outline-warning btn-sm" data-stdrole="<?= $sigcom['rolename'] ?>" data-stdname="<?= $sigcom['name'] ?>" data-stdmatric="<?= $sigcom['student_matric'] ?>" data-toggle="modal" data-target="#delete_orgcom">Delete</a></td>
-                <?php endif ?>
-            </tr>
+        <tr>
+            <td><?= $sigcom['student_matric'] ?></td>
+            <td><?= $sigcom['name'] ?></td>
+            <td><?= $sigcom['rolename'] ?></td>
+            <?php if ($this->session->userdata('isMentor')) : ?>
+            <td><a class="btn btn-outline-warning btn-sm" data-stdrole="<?= $sigcom['rolename'] ?>" data-stdname="<?= $sigcom['name'] ?>" data-stdmatric="<?= $sigcom['student_matric'] ?>"
+                    data-toggle="modal" data-target="#delete_orgcom">Delete</a></td>
+            <?php endif ?>
+        </tr>
         <?php endforeach ?>
     </tbody>
 </table>
@@ -213,7 +182,7 @@
                     <label for="role">Role</label>
                     <select name="role_id" class="form-control" required>
                         <?php foreach ($roles_org as $rorg) : ?>
-                            <option value="<?= $rorg['id'] ?>"><?= $rorg['rolename'] ?></option>
+                        <option value="<?= $rorg['id'] ?>"><?= $rorg['rolename'] ?></option>
                         <?php endforeach ?>
                     </select>
                 </div>
@@ -226,7 +195,7 @@
                     <label for="student_id">Member</label>
                     <select name="student_id" id="student_id" class="form-control" required>
                         <?php foreach ($sig_member as $sm) : ?>
-                            <option value="<?= $sm['id'] ?>"><?= $sm['name'] ?></option>
+                        <option value="<?= $sm['id'] ?>"><?= $sm['name'] ?></option>
                         <?php endforeach ?>
                     </select>
                 </div>
@@ -241,16 +210,16 @@
 </div>
 
 <script>
-    $(document).ready(function() {
-        $('#orgcom_table').DataTable();
-    });
-    var confirmtext = document.getElementById('confirmtext');
-    $('#delete_orgcom').on('show.bs.modal', function(e) {
-        var matric = $(e.relatedTarget).data('stdmatric');
-        var name = $(e.relatedTarget).data('stdname');
-        var rolename = $(e.relatedTarget).data('stdrole');
-        $(e.currentTarget).find('input[name="stdmatric"]').val(matric);
-        $(e.currentTarget).find('input[name="stdname"]').val(name);
-        $(e.currentTarget).find('input[name="stdrole"]').val(rolename);
-    });
+$(document).ready(function() {
+    $('#orgcom_table').DataTable();
+});
+var confirmtext = document.getElementById('confirmtext');
+$('#delete_orgcom').on('show.bs.modal', function(e) {
+    var matric = $(e.relatedTarget).data('stdmatric');
+    var name = $(e.relatedTarget).data('stdname');
+    var rolename = $(e.relatedTarget).data('stdrole');
+    $(e.currentTarget).find('input[name="stdmatric"]').val(matric);
+    $(e.currentTarget).find('input[name="stdname"]').val(name);
+    $(e.currentTarget).find('input[name="stdrole"]').val(rolename);
+});
 </script>
